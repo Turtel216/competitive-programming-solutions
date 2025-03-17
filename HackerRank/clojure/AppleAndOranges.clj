e4c86a0c1135be5e4cd855edc6d45d9e58fc462e
Turtel216 <papadimitrios216@gmail.com>
2025-03-17 18:38:15 +0200

    Add HackerRank Clojure solution

---
+++ HackerRank/clojure/AppleAndOranges.clj
@@ -0,0 +1,24 @@
+;;
+;; Problem Statement from HackerRank
+;;
+;; You can read the full problem state here: https://www.hackerrank.com/challenges/apple-and-orange/problem
+;;
+;; NOTE: This does not run in GHCI, only on hacker rank
+
+
+(defn solve [input]
+  (let [[s t a b m n & rest] input
+        apple-distances (take m rest)
+        orange-distances (drop m rest)
+        apples-in-range (count (filter #(<= s % t) (map #(+ a %) apple-distances)))
+        oranges-in-range (count (filter #(<= s % t) (map #(+ b %) orange-distances)))]
+    [apples-in-range oranges-in-range]))
+
+(let [input (->> (slurp *in*)
+                 (clojure.string/trim)
+                 (clojure.string/split-lines)
+                 (mapcat #(clojure.string/split % #"\s+"))
+                 (map #(Integer/parseInt %)))
+      results (solve input)]
+  (doseq [result results]
+    (println result)))
